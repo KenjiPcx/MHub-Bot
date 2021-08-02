a09c15d8e1788a492add3b11f103d384500c3250
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2021-08-03 02:38:37 +0800

    Implemented add/remove roles from emoji reaction :sparkles:

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,39 @@
-"use strict";
+const { Client, Collection, Intents } = require("discord.js");
+const CONST = require("./constants");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  partials: ["MESSAGE", "REACTION"],
+});
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageReactionAdd", (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+
+  if (member && reaction.message.id === CONST.ROLES_MSG_ID) {
+    const role = CONST.emoji_map.get(name);
+    if (role) {
+      member.roles.add(role).catch(console.log);
+    }
+  }
+});
+
+client.on("messageReactionRemove", (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+
+  if (member && reaction.message.id === CONST.ROLES_MSG_ID) {
+    const role = CONST.emoji_map.get(name);
+    if (role) {
+      member.roles.remove(role).catch(console.log);
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
---
+++ src/embedRolesMsg.js
@@ -0,0 +1,47 @@
+// Use this in on ready to post first time
+// require("./embedRolesMsg").initRolesMsg(client, CONST.ROLES_CHANNEL_ID);
+
+const initRolesMsg = (client, channelId) => {
+  client.channels.cache.get(channelId).send({
+    embed: {
+      color: 3447003,
+      fields: [
+        {
+          name: "Choose Your Roles",
+          value: `
+            Please tell us more about you so we can assign you to the right channel.
+            
+            React to this message with the corresponding emojis to get your roles.
+            You can click and unclick anytime to change your roles.
+
+            Year of Study
+
+            :one: - First Year
+            :two: - Second Year
+            :three: - Third Year
+            :four: - Fourth Year
+            :student: - Graduate
+
+            Field of Study
+
+            :ledger: - Accounting & Finance
+            :chart: - Business Management & Administration
+            :briefcase: - Consultancy
+            :art: - Arts & Design
+            :film_frames:  - Audio & Media Production
+            :computer: - Computing & IT
+            :teacher: - Education
+            :four_leaf_clover:  - Environmental Protection
+            :scales: - Law
+            :label: - Marketing & Sales
+
+          `,
+        },
+      ],
+    },
+  });
+};
+
+module.exports = {
+  initRolesMsg,
+};
