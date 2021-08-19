d48def506448b99be70bf03bb86bf5af38f175fd
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2021-08-19 20:39:13 +0800

    Changed Messages Layout :art:

--- src/embeds/subscribeInterests1.js
+++ src/embeds/subscribeInterests1.js
@@ -1,17 +1,13 @@
-// Use this in on ready to post first time
-// require("../embeds/subscribeInterests1").sendMsg(client, CONST.kROLES_CHANNEL_ID);
-
-const sendMsg = (client, channelId) => {
-  client.channels.cache
-    .get(channelId)
-    .send({
-      embeds: [
-        {
-          color: 3447003,
-          fields: [
-            {
-              name: "Subscribe to our bot to get weekly updates (Part1)",
-              value: `
+const createMsg = () => {
+  return {
+    color: 0x0099ff,
+    title: "Subscribe to our bot to get weekly updates (Part1)",
+    description: `Please tell us more about your interests.
+           `,
+    fields: [
+      {
+        name: `\u200b`,
+        value: `
             Please select what kind of events would you like to get notified.
             
             React to this message with the corresponding emojis.
@@ -34,14 +30,17 @@ const sendMsg = (client, channelId) => {
             :firecracker: - Career Panels
             :white_check_mark: - Save
             `,
-            },
-          ],
-        },
-      ],
-    })
-    .catch(console.log);
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      text: "Malaysian Hub",
+      icon_url:
+        "https://lh3.googleusercontent.com/x8wsnfcrbcrcdMnAN6ji9lxWZzACyQbnl6j1ID61LhN8IDmlcBQP6JgtXQYq392dyR-VOMHXF7f3p70IT9xUwLB1c8rbWO7PtAUB_LhAnRvN751oP4irMFd-9HnHSoy8QYPzlzJc_xWAdoix-OTE826c07yyITGHDCAA45c-O8uiFmPQlSfROSACWFPSKRx35SUzbyMBIOrVKb_jhWGHe6aKzulR7-aMFXfihGWoF-RIi59l2eeFi-JeS9qISaZ-UxvwhHJKRjuj6zFBgYw8XPOj9OTcmiDzaOc57ePV0cqcGzgdSjJUzBR8GDtX3-CZhHPIBSo8dhesUoXd55-iSJB7KJcYVsoNGVhv1C8lLpuit7uHklT1X9XBXmo3lc_m3ynKWXTIAi4ENfEBDTG1kJv0KEN86h1qr8n4hoo-RPRfv1lTqRZ6rtS-xEu0LVxE9tJD75aMGLpsvoBRwQo-H5f3L7b5uBg2iXWR4Hg10CtzsmjFilui1lf1YbbzxrPVq4kbkSOY_VZc-MQg4ieJ72IuopsN5a191UfrGiGhRuPYCOxVYUxrJjVmJMMhTDsTrbMUGuxakD0Nk5Bao3XVCt-eGWniMGIJO_zp9UA5kRveO3GzNgM5xM2XvOLra7UQsdZnbvGzagyfDX01KGgxWfOKbUFvzMX28YKLYgGOyV7tORRCSDU9U-5MC75fBZlnsxnqdWoKy2YMK1gqId4Dl_o=w137-h132-no?authuser=0",
+    },
+  };
 };
 
 module.exports = {
-  sendMsg,
+  createMsg,
 };
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,8 @@
 const { kBOT_ID } = require("../constants");
 const { createMsg } = require("../embeds/rolesMsg");
 const { createMsg: createAcceptMsg } = require("../embeds/acceptRules")
+const { createMsg: createInterest1Msg } = require("../embeds/subscribeInterests1")
+const { createMsg: createInterest2Msg } = require("../embeds/subscribeInterests2")
 
 module.exports = {
   name: "messageCreate",
@@ -22,6 +24,11 @@ module.exports = {
     if (message.content === "Accept Rules") {
       const embed = createAcceptMsg();
       client.channels.cache.get("869252924956618813").send({ embeds: [embed] });
+    }
+    if (message.content === "Interest") {
+      const embed1 = createInterest1Msg()
+      const embed2 = createInterest2Msg()
+      client.channels.cache.get("876016445484597300").send({ embeds: [embed1, embed2] });
     }
   },
 };
